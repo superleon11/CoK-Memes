e1343c694507a2a6e2d21a217538afd5d88432b9
Dave Pringle <davepringle46@yahoo.co.uk>
2023-06-27 14:51:10 +0200

    add ToString override to wooper handler

--- bot/src/ChampionsOfKhazad.Bot/EventHandlers/WooperMessageHandler.cs
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/WooperMessageHandler.cs
@@ -22,4 +22,6 @@ public class WooperMessageHandler : IMessageReceivedEventHandler
             await message.AddReactionsAsync(new[] { _clown, _skull, _nerd });
         }
     }
+
+    public override string ToString() => nameof(WooperMessageHandler);
 }
